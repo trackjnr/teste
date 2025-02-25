97d11a44a0b51e96db435fea5b10cc2fc5809358
trackjnr <lenther32@gmail.com>
2025-02-25 13:45:57 +0100

    premier-comit

--- js/functions/uptate-game.js
+++ js/functions/uptate-game.js
@@ -1,47 +1,4 @@
-import { gameRunning, player, gravity, obstacles, score, gameContainer, ctx } from "../main.js";
-
-
-    if (!gameRunning) return;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    player.dy += gravity;
-    player.y += player.dy;
-
-    if (player.y + player.height > canvas.height) {
-        player.y = canvas.height - player.height;
-        player.dy = 0;
-    }
-
-    ctx.fillStyle = "blue";
-    ctx.fillRect(player.x, player.y, player.width, player.height);
-
-    if (Math.random() < 0.02) {
-        obstacles.push({ x: canvas.width, y: canvas.height - 20, width: 20, height: 20 });
-    }
-
-    ctx.fillStyle = "red";
-    for (let i = 0; i < obstacles.length; i++) {
-        obstacles[i].x -= 3;
-        ctx.fillRect(obstacles[i].x, obstacles[i].y, obstacles[i].width, obstacles[i].height);
-
-        if (
-            player.x < obstacles[i].x + obstacles[i].width &&
-            player.x + player.width > obstacles[i].x &&
-            player.y < obstacles[i].y + obstacles[i].height &&
-            player.y + player.height > obstacles[i].y
-        ) {
-            gameRunning = false;
-            alert("💥 GAME OVER ! Score : " + score);
-            gameContainer.classList.add("hidden");
-            return;
-        }
-    }
-
-    ctx.fillStyle = "black";
-    ctx.font = "16px Arial";
-    ctx.fillText("Score: " + score, 10, 20);
-    score++;
-
+import { gameRunning, player, gravity, obstacles, score, gameContainer, ctx } from "../main.js"
     requestAnimationFrame(updateGame);
 
 
